478f04d2dfcb343b2b478a8d3f10eeec682a95ce
dipen2000 <dipenchavda2000@gmail.com>
2021-11-22 20:54:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+let songDatabase = {
+  "Rock" : [
+    {
+    "Song" : "Sweet child o mine",
+    "Artist" : "Guns n Roses",
+    "imgUrl" : "use this shit",
+    "spotifyLink" : "use this shit"
+    }
+  ],
+};
+
+
+function App() {
+  return (
+    <div className="App">
+        hello I am BATMAN
+    </div>
+  );
+}
+
+export default App;
